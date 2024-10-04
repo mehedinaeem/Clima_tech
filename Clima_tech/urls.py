164cb9c0fdae5b0b31be31ddaae92a1f7f793265
mehedinaeem <mehedinaeem00@gmail.com>
2024-10-04 22:19:51 +0600

    tree complete

--- Clima_tech/urls.py
+++ Clima_tech/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('',include('app_01.urls')),
-    path('',include('app_02.urls')),
+    # path('',include('app_02.urls')),
 ]
